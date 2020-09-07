e3440bf6e71167143cbb42dc3cf23cd6ed9fbce5
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-07 11:15:44 +0100

    Initial commit

---
+++ src/main/java/TermWithExponent.java
@@ -0,0 +1,17 @@
+public class TermWithExponent {
+    final private VariableTerm termInside;
+    final private int exponent;
+
+    public TermWithExponent(VariableTerm termInside, int exponent) {
+        this.termInside = termInside;
+        this.exponent = exponent;
+    }
+
+    public VariableTerm getTermInside() {
+        return termInside;
+    }
+
+    public int getExponent() {
+        return exponent;
+    }
+}
---
+++ src/main/java/VariableTerm.java
@@ -0,0 +1,11 @@
+public class VariableTerm {
+    final private char symbol;
+
+    public VariableTerm(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
