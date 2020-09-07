82f316e4e07757ae236d4795a3d6c52baadf410c
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-07 16:33:07 +0100

    Added class for product of terms

---
+++ src/main/java/ProductOfTerms.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ProductOfTerms {
+    final private ArrayList<Term> theProduct;
+
+    public ProductOfTerms(ArrayList<Term> theProduct) {
+        this.theProduct = theProduct;
+    }
+
+    final ArrayList<Term> getAll() {
+        return theProduct;
+    }
+}
