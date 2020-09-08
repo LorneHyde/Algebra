e603f3c8938b6df874e37d49d5bc19805579602c
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-08 22:05:56 +0100

    Wrote testPlusWhenGivenSimpleTerm for TestProductOfTerms

--- src/test/java/TestProductOfTerms.java
+++ src/test/java/TestProductOfTerms.java
@@ -16,4 +16,19 @@ public class TestProductOfTerms {
         assertEquals(sum, new ProductOfTerms(termSet, 4));
     }
 
+    @Test
+    public void testPlusWhenGivenSimpleTerm() {
+        var termSet = new HashSet<Term>();
+        termSet.add(new SimpleTerm('x'));
+        termSet.add(new SimpleTerm('y', 1, 2));
+        var product1 = new ProductOfTerms(termSet);
+        var simpleTerm = new SimpleTerm('x');
+        var sum = product1.plus(simpleTerm);
+        var expectedTermSet = new HashSet<Term>();
+        expectedTermSet.add(product1);
+        expectedTermSet.add(simpleTerm);
+        var expected_answer = new SumOfTerms(expectedTermSet);
+        assertEquals(sum, expected_answer);
+    }
+
 }
