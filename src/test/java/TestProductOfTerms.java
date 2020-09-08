40246a8d5a3882e514be6467fdc26e29111a1fab
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-08 22:11:03 +0100

    Wrote test for adding 2 incomparable ProductOfTerms objects

--- src/test/java/TestProductOfTerms.java
+++ src/test/java/TestProductOfTerms.java
@@ -31,4 +31,21 @@ public class TestProductOfTerms {
         assertEquals(sum, expected_answer);
     }
 
+    @Test
+    public void testPlusWhenGivenIncomparableProduct() {
+        var termSet1 = new HashSet<Term>();
+        termSet1.add(new SimpleTerm('x'));
+        termSet1.add(new SimpleTerm('y', 1, 2));
+        var termSet2 = new HashSet<Term>();
+        termSet2.add(new SimpleTerm('x'));
+        termSet2.add(new SimpleTerm('y', 1, 3));
+        var product1 = new ProductOfTerms(termSet1);
+        var product2 = new ProductOfTerms(termSet2);
+        var sum = product1.plus(product2);
+        var expectedTermSet = new HashSet<Term>();
+        expectedTermSet.add(product1);
+        expectedTermSet.add(product2);
+        var expected_answer = new SumOfTerms(expectedTermSet);
+        assertEquals(sum, expected_answer);
+    }
 }
