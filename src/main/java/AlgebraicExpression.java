b60fefb00efaaa5c754005188304bee0702dd737
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-09 21:47:07 +0100

    Added header comment for AlgebraicExpression interface

--- src/main/java/AlgebraicExpression.java
+++ src/main/java/AlgebraicExpression.java
@@ -1,3 +1,4 @@
+/** Represents an algebraic expression, which may be either a composite term or a sum of terms.*/
 public interface AlgebraicExpression {
     AlgebraicExpression plus(CompositeTerm s);
     AlgebraicExpression plus(SumOfTerms s);
