7328ccd81a0bc9fcec689e8f83e1a9e42f5366b5
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-10 15:18:40 +0100

    Wrote tests for multiply when given 2 terms with different coefficients, and when given a product of terms and a term with coefficients

--- src/test/java/TestCompositeTerm.java
+++ src/test/java/TestCompositeTerm.java
@@ -15,4 +15,37 @@ public class TestCompositeTerm {
         assertEquals(expected_result, actual_result);
         assertEquals(expected_result, also_actual_result);
     }
+
+    @Test
+    public void testMultiplyReturnsCorrectProductOfTermsWhenGivenTwoTermWithCoefficientsWithDifferentSymbols() {
+        var term1 = new TermWithCoefficient('x', 2, 3);
+        var term2 = new TermWithCoefficient('y', 5, 6);
+        var expected_set = new HashSet<SimpleTerm>();
+        expected_set.add(new SimpleTerm('x', 3));
+        expected_set.add(new SimpleTerm('y', 6));
+        var expected_result = new ProductOfTerms(expected_set, 10);
+        var actual_result = term1.multiply(term2);
+        var also_actual_result = term2.multiply(term1);
+        assertEquals(expected_result, actual_result);
+        assertEquals(expected_result, also_actual_result);
+    }
+
+    @Test
+    public void TestMultiplyReturnsCorrectAnswerWhenGivenProductOfTermsAndTermWithCoefficient() {
+        var term1 = new TermWithCoefficient('x', 2, 3);
+        var term2set = new HashSet<SimpleTerm>();
+        term2set.add(new SimpleTerm('y'));
+        term2set.add(new SimpleTerm('x'));
+        term2set.add(new SimpleTerm('z', 2));
+        var term2 = new ProductOfTerms(term2set, 1);
+        var expectedset = new HashSet<SimpleTerm>();
+        expectedset.add(new SimpleTerm('y'));
+        expectedset.add(new SimpleTerm('x', 4));
+        expectedset.add(new SimpleTerm('z', 2));
+        var expectedResult = new ProductOfTerms(expectedset, 2);
+        var actualResult = term1.multiply(term2);
+        var alsoActualResult = term2.multiply(term1);
+        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult, alsoActualResult);
+    }
 }
