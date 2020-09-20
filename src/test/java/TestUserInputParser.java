3ad870b854935b5d05fd90617f5e18d6f19015b0
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-20 17:30:03 +0100

    Wrote remaining tests for user input parser

--- src/test/java/TestUserInputParser.java
+++ src/test/java/TestUserInputParser.java
@@ -9,10 +9,7 @@ public class TestUserInputParser {
     public void testExpandBracketsWhenGivenSimplifiedExpressionWithoutBrackets() {
         var x = new CompositeTerm('x');
         var y = new CompositeTerm('y');
-        var xPlusYSet = new HashSet<CompositeTerm>();
-        xPlusYSet.add(x);
-        xPlusYSet.add(y);
-        var expectedResult = new SumOfTerms(xPlusYSet);
+        var expectedResult = x.plus(y);
         var actualResult = UserInputParser.expandBrackets("x + y");
         assertEquals(expectedResult, actualResult);
     }
@@ -21,6 +18,55 @@ public class TestUserInputParser {
     public void testExpandBracketsWhenGivenNonSimplifiedSumWithoutBrackets() {
         var expectedResult = new SumOfTerms(new CompositeTerm('x', 2, 1));
         var actualResult = UserInputParser.expandBrackets("x + x");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenSumRaisedToPower() {
+        var expectedResult = UserInputParser.expandBrackets("x^3 + 3x^2y + 3y^2x + y^3");
+        var actualResult = UserInputParser.expandBrackets("(x+y)^3");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenCompositeTermTimesSumOfTerms() {
+        var expectedResult = UserInputParser.expandBrackets("x^2 - 3x");
+        var actualResult = UserInputParser.expandBrackets("x(x-3)");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenPairOfNonNestedBrackets(){
+        var expectedResult = UserInputParser.expandBrackets("x^2 - y^2");
+        var actualResult = UserInputParser.expandBrackets("(x+y)(x-y)");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenSumContainingBracketsThatNeedExpanding() {
+        var expectedResult = new SumOfTerms(new CompositeTerm('x', 1, 2));
+        var actualResult = UserInputParser.expandBrackets("(x+y)(x-y) + y^2");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenNestedPower(){
+        var expectedResult = new SumOfTerms(new CompositeTerm('x', 1, 4));
+        var actualResult = UserInputParser.expandBrackets("((x+y)(x-y) + y^2)^2");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenSumWithBracketsThatShouldEvaluateToZero() {
+        var expectedResult = new SumOfTerms(new CompositeTerm(0));
+        var actualResult = UserInputParser.expandBrackets("(x+1)^2 - (x+1)^2");
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testExpandBracketsWhenGivenComplicatedNestedExpression(){
+        var expectedResult = UserInputParser.expandBrackets("x^3 + x^2 + x + 1");
+        var actualResult = UserInputParser.expandBrackets("x(x(x+1)+1)+1");
         assertEquals(expectedResult, actualResult);
     }
 }
