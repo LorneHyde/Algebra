3964cf80d64ea26e1373bf7c6ce44f8513b94a78
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-20 20:29:43 +0100

    Wrote string to represent input rules

--- src/main/java/UserInputParser.java
+++ src/main/java/UserInputParser.java
@@ -1,13 +1,24 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import javax.xml.transform.ErrorListener;
 import java.util.Scanner;
 
 public class UserInputParser {
-    static boolean hasErrors = false;
-    static String errorMessage = "";
-    static String inputString = "";
+    private final static String rules =
+            "Your input should be a valid algebraic expression, which I will then simplify.\n" +
+                    "This is not the same thing as an equation - note that you can't have the \"=\" sign! \n" +
+                    "Equations with nested brackets are allowed - for instance, \"x(x(x+1)+1)+1\" will simplify to " +
+                    "x^3 + x^2 + x + 1.\n" +
+                    "I can also simplify a sum of terms without brackets " +
+                    "- for instance, \"x + 2x\" will simplify to 3x.\n" +
+                    "I am able to ignore spaces, so \"x+1\" and \"x + 1\" both mean the same to me.\n" +
+                    "I am NOT able to understand any expression which cannot be simplified to a polynomial " +
+                    "(possibly with negative exponents).\n" +
+                    "For instance, I can understand \"x^-1 + xy\" but I cannot understand \"(x+y)^-1\" or \"x^y\".\n" +
+                    "Hence, I will always interpret terms such as x^2y as meaning x^2*y.";
+    private static boolean hasErrors = false;
+    private static String errorMessage = "";
+    private static String inputString = "";
 
     public static void main(String[] args) {
         while (true) {
@@ -19,6 +30,8 @@ public class UserInputParser {
             inputString = scanner.nextLine();
             if (inputString.toLowerCase().equals("quit")) {
                 System.exit(0);
+            } else if (inputString.toLowerCase().equals("rules")) {
+                System.out.println(rules);
             } else {
                 try {
                     AlgebraicExpression a = expandBrackets(inputString);
@@ -28,8 +41,7 @@ public class UserInputParser {
                         System.out.println("Your expression with brackets expanded is:");
                         System.out.println(a);
                     }
-                }
-                catch (RuntimeException e) {
+                } catch (RuntimeException e) {
                     printErrorMessage();
                 }
                 System.out.println();
@@ -41,7 +53,7 @@ public class UserInputParser {
         System.out.println();
         System.out.println(errorMessage);
         System.out.println("Your expression is not valid. To see a list of rules for user input, " +
-                "type \"rules\". To see a list of example inputs, type \"examples\".");
+                "type \"rules\".");
     }
 
     public static AlgebraicExpression expandBrackets(String algebraString) {
