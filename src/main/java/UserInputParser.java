3579abf56d3db5f38ec06512a56c3d5cd60c6af8
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-20 11:24:32 +0100

    Added instructions and an infinite loop in the main function

--- src/main/java/UserInputParser.java
+++ src/main/java/UserInputParser.java
@@ -2,17 +2,30 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 import java.io.InputStream;
+import java.util.Scanner;
 
 public class UserInputParser {
-    public static void main(String[] args) throws Exception {
-        InputStream is = System.in; //TODO: Stop requiring the user to type in ctrl^D or ctrl^Z
-        ANTLRInputStream input = new ANTLRInputStream(is); //TODO: Replace this with something not deprecated.
-        AlgebraLexer lexer = new AlgebraLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        AlgebraParser parser = new AlgebraParser(tokens);
-        ParseTree tree = parser.algebraicExpression();
-        MyAlgebraVisitor expander = new MyAlgebraVisitor();
-        AlgebraicExpression a = expander.visit(tree);
-        System.out.println(a);
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Please enter in an algebraic expression, and I will expand the brackets for you.");
+            System.out.println("If you would like to quit, type \"quit\".");
+            Scanner scanner = new Scanner(System.in);
+            var answer = scanner.nextLine();
+            if (answer.toLowerCase().equals("quit")) {
+                System.exit(0);
+            }
+            else {
+                ANTLRInputStream input = new ANTLRInputStream(answer); //TODO: Replace this with something not deprecated.
+                AlgebraLexer lexer = new AlgebraLexer(input);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                AlgebraParser parser = new AlgebraParser(tokens);
+                ParseTree tree = parser.algebraicExpression();
+                MyAlgebraVisitor expander = new MyAlgebraVisitor();
+                AlgebraicExpression a = expander.visit(tree);
+                System.out.println("Your expression with brackets expanded is:");
+                System.out.println(a);
+                System.out.println();
+            }
+        }
     }
 }
