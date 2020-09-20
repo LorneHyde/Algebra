118d3aaa308cb77f84f67b92329a8b5f43450464
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-20 19:49:15 +0100

    replaced ErrorListener with a version more suited to my context

--- src/main/java/UserInputParser.java
+++ src/main/java/UserInputParser.java
@@ -1,40 +1,72 @@
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import javax.xml.transform.ErrorListener;
 import java.util.Scanner;
 
 public class UserInputParser {
+    static boolean hasErrors = false;
+    static String errorMessage = "";
+    static String inputString = "";
+
     public static void main(String[] args) {
         while (true) {
+            hasErrors = false;
+            errorMessage = "";
             System.out.println("Please enter in an algebraic expression, and I will expand the brackets for you.");
             System.out.println("If you would like to quit, type \"quit\".");
             Scanner scanner = new Scanner(System.in);
-            var inputString = scanner.nextLine();
+            inputString = scanner.nextLine();
             if (inputString.toLowerCase().equals("quit")) {
                 System.exit(0);
             } else {
                 try {
                     AlgebraicExpression a = expandBrackets(inputString);
-                    System.out.println("Your expression with brackets expanded is:");
-                    System.out.println(a);
+                    if (hasErrors) {
+                        printErrorMessage();
+                    } else {
+                        System.out.println("Your expression with brackets expanded is:");
+                        System.out.println(a);
+                    }
                 }
                 catch (RuntimeException e) {
-                    System.out.println("Your expression is not valid. To see a list of rules for user input, " +
-                            "type \"rules\". To see a list of example inputs, type \"examples\".");
+                    printErrorMessage();
                 }
                 System.out.println();
             }
         }
     }
 
+    private static void printErrorMessage() {
+        System.out.println();
+        System.out.println(errorMessage);
+        System.out.println("Your expression is not valid. To see a list of rules for user input, " +
+                "type \"rules\". To see a list of example inputs, type \"examples\".");
+    }
+
     public static AlgebraicExpression expandBrackets(String algebraString) {
-        var inputCharStream = CharStreams.fromString(algebraString);
+        CodePointCharStream inputCharStream = CharStreams.fromString(algebraString);
+        MeaningfulErrorListener errorListener = new MeaningfulErrorListener();
         AlgebraLexer lexer = new AlgebraLexer(inputCharStream);
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(errorListener);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         AlgebraParser parser = new AlgebraParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(errorListener);
         ParseTree tree = parser.algebraicExpression();
         MyAlgebraVisitor expander = new MyAlgebraVisitor();
         return expander.visit(tree);
+    }
+
+    private static class MeaningfulErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+            if (!hasErrors) {
+                hasErrors = true;
+                errorMessage += String.format("I was rather confused to see '%s' at position %d of your input. ",
+                        inputString.charAt(charPositionInLine), charPositionInLine);
+            }
+        }
     }
 }
