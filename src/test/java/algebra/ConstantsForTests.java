111bcf329249a46653b724aabe87ff7e59e142e9
Lorne Hyde <lorne.ab.hyde@gmail.com>
2020-09-22 21:26:28 +0100

    Refactored tests in TestMultiplyForCompositeTerm

--- src/test/java/algebra/ConstantsForTests.java
+++ src/test/java/algebra/ConstantsForTests.java
@@ -6,16 +6,27 @@ public class ConstantsForTests {
     public static final CompositeTerm one = new CompositeTerm(1);
     public static final CompositeTerm three = new CompositeTerm(3);
     public static final CompositeTerm four = new CompositeTerm(4);
+    public static final CompositeTerm twelve = new CompositeTerm(12);
     public static final CompositeTerm x = new CompositeTerm('x');
     public static final CompositeTerm two_x = new CompositeTerm('x', 2, 1);
     public static final CompositeTerm three_x = new CompositeTerm('x', 3, 1);
+    public static final CompositeTerm six_x = new CompositeTerm('x', 6, 1);
     public static final CompositeTerm xSquared = new CompositeTerm('x', 1, 2);
+    public static final CompositeTerm two_xSquared = new CompositeTerm('x', 2, 2);
+    public static final CompositeTerm three_xToThePowerOfMinus2 = new CompositeTerm('x', 3, -2);
+    public static final CompositeTerm five_xCubed = new CompositeTerm('x', 5, 3);
+    public static final CompositeTerm ten_xToTheFive = new CompositeTerm('x', 10, 5);
     public static final CompositeTerm y = new CompositeTerm('y');
+    public static final CompositeTerm three_yCubed = new CompositeTerm('y', 3, 3);
     public static final CompositeTerm z = new CompositeTerm('z');
     public static final CompositeTerm xyCubed = get_xyCubed();
+    public static final CompositeTerm six_xSquared_yCubed = get_six_xSquared_yCubed();
     public static final CompositeTerm xySquared = get_xySquared();
     public static final CompositeTerm three_xySquared = new CompositeTerm(xySquared.getSet(), 3);
     public static final CompositeTerm four_xySquared = new CompositeTerm(xySquared.getSet(), 4);
+    public static final CompositeTerm two_w_xSquared_yCubed = get_two_w_xSquared_yCubed();
+    public static final CompositeTerm three_x_z_yToTheEight = get_three_x_z_yToTheEight();
+    public static final CompositeTerm w_z_xCubed_yToTheEleven = get_w_z_xCubed_yToTheEleven();
 
     public static final SumOfTerms x_plus_three = get_x_plus_three();
     public static final SumOfTerms x_plus_four = get_x_plus_four();
@@ -28,6 +39,7 @@ public class ConstantsForTests {
     public static final SumOfTerms y_plus_z_plus_xSquared_plus_three_x = get_y_plus_z_plus_xSquared_plus_two_x();
     public static final SumOfTerms x_plus_xySquared = get_x_plus_xySquared();
     public static final SumOfTerms xySquared_plus_xyCubed = get_xySquared_plus_xyCubed();
+
 
     private static SumOfTerms get_x_plus_y() {
         HashSet<CompositeTerm> sumSet = new HashSet<>();
@@ -104,6 +116,13 @@ public class ConstantsForTests {
         return new CompositeTerm(termSet);
     }
 
+    private static CompositeTerm get_six_xSquared_yCubed() {
+        var termSet = new HashSet<SimpleTerm>();
+        termSet.add(new SimpleTerm('x', 2));
+        termSet.add(new SimpleTerm('y', 3));
+        return new CompositeTerm(termSet, 6);
+    }
+
     private static SumOfTerms get_xySquared_plus_xyCubed() {
         var sumSet = new HashSet<CompositeTerm>();
         sumSet.add(xySquared);
@@ -123,5 +142,30 @@ public class ConstantsForTests {
         sumSet.add(four);
         sumSet.add(x);
         return new SumOfTerms(sumSet);
+    }
+
+    private static CompositeTerm get_two_w_xSquared_yCubed() {
+        var termSet = new HashSet<SimpleTerm>();
+        termSet.add(new SimpleTerm('w'));
+        termSet.add(new SimpleTerm('x', 2));
+        termSet.add(new SimpleTerm('y', 3));
+        return new CompositeTerm(termSet, 2);
+    }
+
+    private static CompositeTerm get_three_x_z_yToTheEight() {
+        var termSet2 = new HashSet<SimpleTerm>();
+        termSet2.add(new SimpleTerm('x'));
+        termSet2.add(new SimpleTerm('y', 8));
+        termSet2.add(new SimpleTerm('z'));
+        return new CompositeTerm(termSet2, 3);
+    }
+
+    private static CompositeTerm get_w_z_xCubed_yToTheEleven() {
+        var expectedTermSet = new HashSet<SimpleTerm>();
+        expectedTermSet.add(new SimpleTerm('w'));
+        expectedTermSet.add(new SimpleTerm('x', 3));
+        expectedTermSet.add(new SimpleTerm('y', 11));
+        expectedTermSet.add(new SimpleTerm('z'));
+        return new CompositeTerm(expectedTermSet, 6);
     }
 }
--- src/test/java/algebra/TestMultiplyForCompositeTerm.java
+++ src/test/java/algebra/TestMultiplyForCompositeTerm.java
@@ -1,111 +1,77 @@
 package algebra;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static algebra.ConstantsForTests.*;
 
 public class TestMultiplyForCompositeTerm {
     @Test
     public void testMultiplyReturnsCorrectTermWhenGivenTwoSingleVariableTermsWithSameSymbol() {
-        var term1 = new CompositeTerm('x', 2, 3);
-        var term2 = new CompositeTerm('x', 5, 6);
-        var expected_result = new CompositeTerm('x', 10, 9);
-
-        var actual_result = term1.multiply(term2);
-        var also_actual_result = term2.multiply(term1);
-
-        assertEquals(expected_result, actual_result);
-        assertEquals(expected_result, also_actual_result);
+        //act
+        var answer = two_xSquared.multiply(five_xCubed);
+        var alsoAnswer = five_xCubed.multiply(two_xSquared);
+        //assert
+        assertEquals(ten_xToTheFive, answer);
+        assertEquals(ten_xToTheFive, alsoAnswer);
     }
 
     @Test
     public void testMultiplyReturnsCorrectTermsWhenGivenTwoSingleVariableTermsWithDifferentSymbols() {
-        var term1 = new CompositeTerm('x', 2, 3);
-        var term2 = new CompositeTerm('y', 5, 6);
-        var expected_set = new HashSet<SimpleTerm>();
-        expected_set.add(new SimpleTerm('x', 3));
-        expected_set.add(new SimpleTerm('y', 6));
-        var expected_result = new CompositeTerm(expected_set, 10);
-        var actual_result = term1.multiply(term2);
-        var also_actual_result = term2.multiply(term1);
-        assertEquals(expected_result, actual_result);
-        assertEquals(expected_result, also_actual_result);
+        //act
+        var actual_result = two_xSquared.multiply(three_yCubed);
+        var also_actual_result = three_yCubed.multiply(two_xSquared);
+        //assert
+        assertEquals(six_xSquared_yCubed, actual_result);
+        assertEquals(six_xSquared_yCubed, also_actual_result);
     }
 
     @Test
     public void testMultiplyReturnsCorrectNumberWhenGivenTwoSingleVariableTermsWithSameSymbolOppositeExponents() {
-        var xSquared = new CompositeTerm('x', 1, 2);
-        var three_xToThePowerOfMinus2 = new CompositeTerm('x', 3, -2);
-        var three = new CompositeTerm(3);
-        var shouldBeThree = xSquared.multiply(three_xToThePowerOfMinus2);
-        var alsoShouldBeThree = three_xToThePowerOfMinus2.multiply(xSquared);
-        assertEquals(three, shouldBeThree);
-        assertEquals(three, alsoShouldBeThree);
+        //act
+        var answer = xSquared.multiply(three_xToThePowerOfMinus2);
+        var alsoAnswer = three_xToThePowerOfMinus2.multiply(xSquared);
+        //assert
+        assertEquals(three, answer);
+        assertEquals(three, alsoAnswer);
     }
 
     @Test
     public void TestMultiplyReturnsCorrectAnswerWhenGivenSingleVariableTermAndMultiVariableTerm() {
-        var term1 = new CompositeTerm('x', 2, 3);
-        var term2set = new HashSet<SimpleTerm>();
-        term2set.add(new SimpleTerm('y'));
-        term2set.add(new SimpleTerm('x'));
-        term2set.add(new SimpleTerm('z', 2));
-        var term2 = new CompositeTerm(term2set, 1);
-        var expectedset = new HashSet<SimpleTerm>();
-        expectedset.add(new SimpleTerm('y'));
-        expectedset.add(new SimpleTerm('x', 4));
-        expectedset.add(new SimpleTerm('z', 2));
-        var expectedResult = new CompositeTerm(expectedset, 2);
-        var actualResult = term1.multiply(term2);
-        var alsoActualResult = term2.multiply(term1);
-        assertEquals(expectedResult, actualResult);
-        assertEquals(expectedResult, alsoActualResult);
+        //act
+        var answer = xyCubed.multiply(six_x);
+        var alsoAnswer = six_x.multiply(xyCubed);
+        //assert
+        assertEquals(six_xSquared_yCubed, answer);
+        assertEquals(six_xSquared_yCubed, alsoAnswer);
     }
 
     @Test
     public void TestMultiplyWhenGivenTwoMultiVariableTerms() {
-        var termSet1 = new HashSet<SimpleTerm>();
-        termSet1.add(new SimpleTerm('w'));
-        termSet1.add(new SimpleTerm('x', 2));
-        termSet1.add(new SimpleTerm('y', 3));
-        var termSet2 = new HashSet<SimpleTerm>();
-        termSet2.add(new SimpleTerm('x'));
-        termSet2.add(new SimpleTerm('y', 8));
-        termSet2.add(new SimpleTerm('z'));
-        var expectedTermSet = new HashSet<SimpleTerm>();
-        expectedTermSet.add(new SimpleTerm('w'));
-        expectedTermSet.add(new SimpleTerm('x', 3));
-        expectedTermSet.add(new SimpleTerm('y', 11));
-        expectedTermSet.add(new SimpleTerm('z'));
-        var product1 = new CompositeTerm(termSet1, 2);
-        var product2 = new CompositeTerm(termSet2, 3);
-        var expectedProduct = new CompositeTerm(expectedTermSet, 6);
-        var actualResult = product1.multiply(product2);
-        var alsoActualResult = product2.multiply(product1);
-        assertEquals(expectedProduct, actualResult);
-        assertEquals(expectedProduct, alsoActualResult);
+        //act
+        var actualResult = two_w_xSquared_yCubed.multiply(three_x_z_yToTheEight);
+        var alsoActualResult = three_x_z_yToTheEight.multiply(two_w_xSquared_yCubed);
+        //assert
+        assertEquals(w_z_xCubed_yToTheEleven, actualResult);
+        assertEquals(w_z_xCubed_yToTheEleven, alsoActualResult);
     }
 
     @Test
     public void testMultiplyWhenGivenNumber() {
-        var x = new CompositeTerm('x');
-        var two = new CompositeTerm(2);
-        var expectedResult = new CompositeTerm('x', 2, 1);
-        var product = x.multiply(two);
-        var alsoProduct = two.multiply(x);
-        assertEquals(expectedResult, product);
-        assertEquals(expectedResult, alsoProduct);
+        //act
+        var answer = x.multiply(three);
+        var alsoAnswer = three.multiply(x);
+        //assert
+        assertEquals(three_x, answer);
+        assertEquals(three_x, alsoAnswer);
     }
 
     @Test
     public void testMultiplyOnTwoNumbers() {
-        var three = new CompositeTerm(3);
-        var five = new CompositeTerm(5);
-        var fifteen = new CompositeTerm(15);
-        var three_times_five = three.multiply(five);
-        var five_times_three = five.multiply(three);
-        assertEquals(fifteen, three_times_five);
-        assertEquals(fifteen, five_times_three);
+        //act
+        var answer = three.multiply(four);
+        var alsoAnswer = four.multiply(three);
+        //assert
+        assertEquals(twelve, answer);
+        assertEquals(twelve, alsoAnswer);
     }
 }
